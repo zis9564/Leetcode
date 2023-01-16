b69945725768a9a5a4ba217967bd4b73f232d0c3
zis9564 <zis9564@gmail.com>
2023-01-16 15:01:40 +0300

    SingleNumber

---
+++ src/main/java/org/example/SingleNumber.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
