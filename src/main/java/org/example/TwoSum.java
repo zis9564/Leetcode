35a7516651b7fea9a597829c9d5f2d1d8da685d4
zis9564 <zis9564@gmail.com>
2023-01-16 15:03:02 +0300

    TwoSum

---
+++ src/main/java/org/example/TwoSum.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++) {
+            Integer complimentInd = map.get(nums[i]);
+            if(complimentInd != null)
+                return new int[]{complimentInd,i};
+            map.put(target-nums[i], i);
+        }
+        return nums;
+    }
+}
